90ae3b8d7656e4f12ced5c13d1d66c2650d494aa
Benoit Damiani <damiani.benoit.jp@gmail.com>
2020-04-08 15:57:59 +0200

    add sigmoid to word2vec model

--- models/word2vec_model.py
+++ models/word2vec_model.py
@@ -4,6 +4,14 @@ from indexing.collection_stat import CollectionStatistics
 from models.query import Query
 from models.weighting import get_idf, get_normalized_log_tf
 from gensim.models import KeyedVectors
+from math import exp
+
+
+def sigmoid(x):
+
+    """Sigmoid function used to discriminate further between similarities below 0.5 and above 0.5"""
+
+    return 1 / (1 + exp(-x))
 
 
 def query_word2vec_model(query: Query, inverted_index: FrequencyInvertedIndex, id_map: IdMap,
@@ -30,15 +38,17 @@ def query_word2vec_model(query: Query, inverted_index: FrequencyInvertedIndex, i
             norm_query = norm_query + w_term_query * w_term_query
             similar_terms = word2vec_vectors.most_similar(term)  # top 10 similar terms based on word2vec model
             similar_terms.append((term, 1))  # add the original term to the list
+            word2vec_normalization = sum([sigmoid(similarity) for _, similarity in similar_terms])
             # weighting similar to vectorial model but adding word2vec similarity for each similar term
             for similar_term, similarity in similar_terms:
+                print(similarity)
                 if similar_term in inverted_index:
                     for document_id in inverted_index[similar_term].keys():
                         if document_id not in relevant_docs:
                             relevant_docs[document_id] = 0.
                         w_term_doc = get_normalized_log_tf(similar_term, document_id, inverted_index, collection_stats)
                         w_term_doc *= get_idf(similar_term, inverted_index, collection_stats)
-                        w_term_doc *= similarity
+                        w_term_doc *= sigmoid(similarity) / word2vec_normalization
                         relevant_docs[document_id] += w_term_doc * w_term_query
     # Final sorting
     ordered_relevant_docs = []
