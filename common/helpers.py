f02605a8faf40dfe199f2cdf76aa43b416af9123
Benoit Damiani <damiani.benoit.jp@gmail.com>
2020-04-08 15:11:28 +0200

    move parse document function

--- common/helpers.py
+++ common/helpers.py
@@ -1,6 +1,21 @@
 import os
 import pickle
-from typing import Iterator, Tuple, Any
+from typing import Iterator, Tuple, Any, List
+
+
+# DOCUMENT PARSING
+
+def parse_document(document_absolute_path: str) -> List[str]:
+    """Given the absolute path to the document, returns the list of the filtered and tokenized words, in their order of appearance.
+    For now, as the words are already tokenized and there is no stop word list, it just split every line of the file."""
+    result = []
+    with open(document_absolute_path, 'r') as f:
+        line = f.readline()
+        while line:
+            parsed = line.split()
+            result += parsed
+            line = f.readline()
+    return result
 
 
 def generate_file_paths(main_directory: str) -> Iterator[Tuple[str, str]]:
